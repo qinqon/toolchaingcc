84f4882f8b97b95473e8f7e38bf267dd89a4c82b
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-09-06 18:46:14 +0200

    added whole archive option
    Change-Id: I7b6aefa892c493b58bbda15c1fed40a1e1aff70b

--- lib/cxxproject_gcctoolchain/plugin.rb
+++ lib/cxxproject_gcctoolchain/plugin.rb
@@ -39,7 +39,9 @@ cxx_plugin do |cxx,bbs,log|
         :EXE_FLAG => "-o",
         :LIB_FLAG => "-l",
         :LIB_PATH_FLAG => "-L",
-        :ERROR_PARSER => Cxxproject::GCCLinkerErrorParser.new
+        :ERROR_PARSER => Cxxproject::GCCLinkerErrorParser.new,
+        :START_OF_WHOLE_ARCHIVE => '-Wl,--whole-archive',
+        :END_OF_WHOLE_ARCHIVE => '-Wl,--no-whole-archive'
       },
     :ARCHIVER =>
       {
