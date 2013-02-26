a9591ee0306a5a3e49acd607aa5fcdb3e25a45c3
Félix Enrique Llorente pastora <felix.enrique.llorente.pastora@ericsson.com>
2013-02-26 15:36:14 +0100

    Remove helpers from plugin.rb

--- lib/cxxproject_gcctoolchain/plugin.rb
+++ lib/cxxproject_gcctoolchain/plugin.rb
@@ -1,99 +1,10 @@
 
-class SharedLibsHelper
-  def symlink_lib_to(link, bb)
-   file = File.basename(bb.executable_name)
-   if file != link 
-     cd "#{bb.output_dir}/libs" do
-       symlink(file, link)
-     end            
-   end
-  end
-end
-
-class OsxSharedLibs < SharedLibsHelper
-  def calc(linker, bb)
-    flags = ['-install_name', bb.get_output_name(linker)]
-    if bb.compatibility != nil
-      flags << '-compatibility_version'
-      flags << bb.compatibility
-    end
-    if bb.minor != nil
-      flags << '-current_version'
-      flags << bb.minor
-    end
-    flags
-  end
-   
-  # For :major=>A, minor=>1.0.1, compatibility=>1.0.0 basic is 'libfoo.A.so'
-  def get_basic_name(linker, bb)
-   prefix = bb.get_output_prefix(linker)
-   name = bb.name
-   dylib = bb.shared_suffix linker
-   return "#{prefix}#{name}#{dylib}"
-  end
-
-
-  # Some symbolic links
-  # ln -s foo.dylib foo.A.dylib
-  def post_link_hook(linker, bb)
-    basic_name = get_basic_name(linker, bb)
-    symlink_lib_to(basic_name, bb)
-  end
-
-  def get_version_suffix(linker, bb)
-    "#{(bb.major ? ".#{bb.major}" : '')}"
-  end
-end
-
-class UnixSharedLibs < SharedLibsHelper
-  
-  # For :major=>1, minor=>2 fullname is '1.2.so'
-  def get_version_suffix(linker, bb)
-    "#{major_suffix bb}#{[bb.major, bb.minor].compact.join('.')}" 
-  end
-
-  def major_suffix(bb)
-    "#{(bb.major ? ".#{bb.major}" : '')}"
-  end
-
-  # For :major=>1, minor=>2 soname is 'libfoo.1.so'
-  #def get_major(linker)
-  # prefix = get_output_prefix(linker)
-  # return "#{prefix}#{name}#{major_suffix}#{shared_suffix linker}"
-  #end
-
-  def get_soname(linker, bb)
-   prefix = bb.get_output_prefix(linker)
-    "#{prefix}#{bb.name}#{major_suffix bb}#{bb.shared_suffix linker}"
-  end
-
-  def calc(linker, bb)
-    return ["-Wl,-soname,#{get_soname(bb,linker)}"]
-  end
-   
-  # For :major=>1, minor=>2 fullname is 'libfoo.so'
-  def get_basic_name(linker, bb)
-   prefix = bb.get_output_prefix(linker)
-   return "#{prefix}#{bb.name}#{bb.shared_suffix(linker)}"
-  end
-
-  # Some symbolic links
-  # ln -s libfoo.so libfoo.1.2.so
-  # ln -s libfoo.1.so libfoo.1.2.so
-  def post_link_hook(linker, bb)
-      basic_name = get_basic_name(linker, bb)
-      soname = get_soname(linker, bb)
-      symlink_lib_to(basic_name, bb)
-      symlink_lib_to(soname, bb)
-  end
-end
-
 cxx_plugin do
 
   require 'errorparser/gcc_compiler_error_parser'
   require 'errorparser/gcc_linker_error_parser'
+  require 'helpers/helpers'
   gccCompilerErrorParser = Cxxproject::GCCCompilerErrorParser.new
-
   prefix = ENV['USE_CCACHE'] ? 'ccache' : nil
   toolchain "gcc",
     :COMPILER =>
@@ -135,7 +46,7 @@ cxx_plugin do
         :ERROR_PARSER => Cxxproject::GCCLinkerErrorParser.new,
         :START_OF_WHOLE_ARCHIVE => {:UNIX => '-Wl,--whole-archive', :OSX => '-force_load', :WINDOWS => '-Wl,--whole-archive'},
         :END_OF_WHOLE_ARCHIVE => {:UNIX => '-Wl,--no-whole-archive', :OSX => '', :WINDOWS => '-Wl,--no-whole-archive'},
-        :ADDITIONAL_COMMANDS => {:OSX => OsxSharedLibs.new, :UNIX => UnixSharedLibs.new},
+        :ADDITIONAL_COMMANDS => {:OSX => Cxxproject::OsxSharedLibs.new, :UNIX => Cxxproject::UnixSharedLibs.new},
         :ADDITIONAL_OBJECT_FILE_FLAGS => {:OSX => [], :UNIX => ['-fPIC']}
       },
     :ARCHIVER =>
